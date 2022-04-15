2250acdc8441e593e90c0d39d58527819cb5497b
thereselau1902 <thereselau1902@gmail.com>
2022-04-15 15:50:19 +0800

    updated user auth logic

--- index.js
+++ index.js
@@ -20,39 +20,84 @@ const pgConnectionConfigs = {
 };
 const pool = new Pool(pgConnectionConfigs);
 
+// Auth stuff
+
+const SALT = ''
+
+const getHash = (input) => {
+  // create new SHA object
+  const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+  // create an unhashed cookie string based on user ID and salt
+  const unhashedString = `${input}-${SALT}`;
+  // generate a hashed cookie string using SHA object
+  shaObj.update(unhashedString);
+  return shaObj.getHash('HEX');
+};
+
+const loginCheck = (req, res, next) => {
+  if (!req.cookies.userIdHash) {
+    res.render('error', { message: 'Please log in to continue.' });
+  }
+  req.isUserLoggedIn = false; // default value
+
+  const idHash = getHash(req.cookies.userId);
+
+  if(req.cookies.userIdHash === idHash){
+    req.isUserLoggedIn = true;
+    res.locals.userId = req.cookies.userIdHash; // pass userId of the user into middleware (as hashed).
+  }
+  next();
+};
+
 app.get('/', (req, res) => {
   res.render('index')
   })
 
 app.post('/tasklist/list', async (req, res) => { // async gives access to await - wait for function to complete before it continues
   try {
-    console.log('tasklist submisison request came in');
-
     const inputs = req.body;
-    console.log('inputs:', inputs)
+    // let taskCookieObj = { task_list_id: 0, task_ids_array: []};
+    const taskCookieObj = { task_ids_array: [] };
 
-    // cookies here maybe
-
-    const listQueryString = `INSERT INTO task_lists (list_name, list_description) VALUES ($1, $2) RETURNING id`;
+    let listQueryString = `INSERT INTO task_lists (list_name, list_description) VALUES ($1, $2) RETURNING id`;
     const listQueryValues = [ inputs['list-name'], inputs['list-description'] ]
-    
     const taskQueryArr = [inputs['task-1'], inputs['task-2'], inputs['task-3']]
+
+    // edit query to include userID if user is logged in
+    console.log('req.cookies', req.cookies)
+    if(req.cookies.userID){
+      console.log('ID cookie found. ID:', req.cookies.userID)
+      listQueryValues.push(req.cookies.userID)
+      console.log('listQueryValues edited:', listQueryValues)
+      listQueryString = `INSERT INTO task_lists (list_name, list_description, assigned_user) VALUES ($1, $2, $3) RETURNING id`
+      console.log('listQueryString edited')
+    }
 
     pool
       .query(listQueryString, listQueryValues)
       .then ((result) => {
         // console.log(result)
         const latestTasklistID = result['rows'][0]['id'];
-        console.log("latestTasklistID is:", latestTasklistID);
-        taskQueryArr.forEach((element) => {
+        // console.log("latestTasklistID is:", latestTasklistID);
+        taskCookieObj['task_list_id'] = latestTasklistID // why isnt this updating :(
+
+        taskQueryArr.forEach((taskElement) => {
           const taskQueryString = `INSERT INTO tasks (list_id, task_name) VALUES ($1, $2)`;
           console.log("inserting tasks under listID:", latestTasklistID);
-          const taskQueryValues = [ latestTasklistID, element ];
+          const taskQueryValues = [ latestTasklistID, taskElement ];
+          taskCookieObj['task_ids_array'].push(taskElement)
+          console.log(taskCookieObj)
           pool.query(taskQueryString, taskQueryValues);
         })
       })
+    console.log('taskCookieObj:', taskCookieObj)
+    console.log('taskCookieObj[task_ids_array]:', taskCookieObj.task_ids_array)
+    /// basically tasklistobj is not being updated as expected
+    res.cookie('sessionTasks', taskCookieObj)
+
     res.redirect('/');
   } catch (err) {
+    console.log('ERROR CAUGHT')
     console.error(err.message)
   }
   
@@ -64,16 +109,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
   console.log('attempting POST')
-
-  // i could probably make the hashing stuff into a separate function
-  // initialise the SHA object
-  const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
-  console.log('SHA objecct created\n')
-  console.log('req.body:',req.body)
-  // input the password from the request to the SHA object
-  shaObj.update(req.body.password);
-  // get the hashed password as output from the SHA object
-  const hashedPassword = shaObj.getHash('HEX');
+  const hashedPassword = getHash(req.body.password)
   
   // store the hashed password in our DB
   const queryString = 'INSERT INTO users (user_name, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)' // to show ourselves what data was sent
@@ -82,12 +118,11 @@ app.post('/register', (req, res) => {
   pool
     .query(queryString, values)
     .then ((result) => {
-      // console.log('User insert query done. result:', result);
-      // console.log('hashing OK. redirecting...')
       res.redirect('/login')
     })
     .catch ((error) => {
-    console.log(error);
+    console.log('ERROR CAUGHT')
+    console.log(error.message);
   });
   // use app.locals to define your own 'global variables' in EJS files. useful for things like navbears where you want to have dynamic content like updating a welcome message with the user's name.
 })
@@ -99,37 +134,39 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
   // retrieve the user entry using their email
   const usernameQueryvalues = [req.body.user_name];
-  pool.query('SELECT * from users WHERE user_name=$1', usernameQueryvalues, (error, result) => {
-    // console.log(result.rows);
-    if (error) {
-      console.log('Error executing query', error.stack);
-      res.status(503).send(result.rows);
-      return;
-    }
+  pool
+  .query('SELECT * from users WHERE user_name=$1', usernameQueryvalues)
+  .then((result) => {
+
     if (result.rows.length === 0) { // not a valid username
       res.status(403).send('login failed!');
       return;
     }
 
-  const retrievedUserInfo = result.rows[0];
+    const retrievedUserInfo = result.rows[0];
+    const hashedPassword = getHash(req.body.password)
 
-  const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
-  // input the password from the request to the SHA object
-  shaObj.update(req.body.password);
-  // get the hashed password as output from the SHA object
-  const hashedPassword = shaObj.getHash('HEX');
-
-  if(retrievedUserInfo.password !== hashedPassword){
-     res.status(403).send('Invalid Username or Password.');
-      return;
-  }
-  res.cookie('userID', retrievedUserInfo.id)
-  // redirect on login success
-  res.redirect('/');
+    if(retrievedUserInfo.password !== hashedPassword){
+      res.status(403).send('Invalid Username or Password.');
+        return;
+    }
+    res.cookie('userID', retrievedUserInfo.id)
+    res.cookie('userIdHash', getHash(retrievedUserInfo.id))
+    // redirect on login success
+    res.redirect('/');
+  })
+  .catch ((error) => {
+    console.log('Error executing query', error.stack);
+    res.status(503).send(result.rows);
+    return;
   });
-});
+  });
 
-app.get('/profile', (req, res) => {
+
+app.get('/profile', loginCheck, (req, res) => {
+  if (req.isUserLoggedIn === false) { // test from loginCheck middleware
+    res.status(403).send('please log in.');
+  }
   res.render('profile')
 })
 
